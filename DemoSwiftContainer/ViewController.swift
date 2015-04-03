e72871f3b9bea875edf4631b1c048ec547af8e9e
Julien DAUPHANT <judauphant@gmail.com>
2015-04-03 12:36:36 +0200

    Initial Commit

---
+++ DemoSwiftContainer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoSwiftContainer
+//
+//  Created by Julien DAUPHANT on 03/04/15.
+//  Copyright (c) 2015 Siz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
